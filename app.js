f0dd5f8746489a28b479a10ba9676c809b35a903
Miles <smbradtmichael@gmail.com>
2021-10-06 19:35:59 -0700

    working buttons over server

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+    socket.on('gamepad type', (id) => {
+        console.log('gamepad: ' + id);
+        io.emit('gamepad type', id)
+    });
+
+    socket.on('buttons pushed', (controller) => {
+        io.emit('buttons pushed', controller)
+    })
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
 
